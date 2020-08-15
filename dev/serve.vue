1d2ce871bb75246b45a4d6953b6aa84c25813320
Trenton Kennedy <tkennedy@idonate.com>
2020-08-14 20:17:30 -0500

    initial npm module

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { DonateUiKitSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    DonateUiKitSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <donate-ui-kit-sample />
+  </div>
+</template>
